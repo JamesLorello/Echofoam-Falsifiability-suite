67c03f77cbe64d82e23615602b27e8ce5ede79cd
James Lorello <hod_baker@yahoo.com>
2025-05-25 05:28:26 -0700

    Add tests for create_animation functions

---
+++ tests/test_simulations.py
@@ -0,0 +1,27 @@
+import unittest
+from matplotlib.figure import Figure
+from matplotlib.animation import FuncAnimation
+
+class TestCreateAnimation(unittest.TestCase):
+    def _check_module(self, module_name):
+        mod = __import__(module_name)
+        if not hasattr(mod, "create_animation"):
+            self.fail(f"{module_name}.create_animation does not exist")
+        fig, anim = mod.create_animation()
+        self.assertIsInstance(fig, Figure)
+        self.assertIsInstance(anim, FuncAnimation)
+
+    def test_simulation(self):
+        self._check_module("simulation")
+
+    def test_teleportation(self):
+        self._check_module("teleportation")
+
+    def test_weather_simulation(self):
+        self._check_module("weather_simulation")
+
+    def test_laser_filamentation(self):
+        self._check_module("laser_filamentation")
+
+if __name__ == "__main__":
+    unittest.main()
